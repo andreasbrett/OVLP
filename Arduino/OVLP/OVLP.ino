d80b17cae86fe332168c2a58ea1e800044c05aba
Andreas Brett <andreasbrett@users.noreply.github.com>
2020-04-01 20:56:13 +0200

    Add files via upload

---
+++ Arduino/OVLP/OVLP.ino
@@ -0,0 +1,290 @@
+/* ----------------------------------------------------------------------------------------------
+   include keyboard library
+   ----------------------------------------------------------------------------------------------
+*/
+#include "Keyboard.h"
+
+
+/* ----------------------------------------------------------------------------------------------
+   define pin <-> button mappings
+   ----------------------------------------------------------------------------------------------
+*/
+#define button_Speed 5
+#define button_Loop 6
+#define button_Back 7
+#define button_PlayPause 8
+#define button_Forward 9
+
+
+/* ----------------------------------------------------------------------------------------------
+   global variables
+   ----------------------------------------------------------------------------------------------
+*/
+long longPressTime = 1250;        // how long is a long press in msec?
+
+bool active_Speed = false;        // speed button press active?
+bool active_Speed_LP = false;     // speed button long-press active?
+long timer_Speed = 0;             // speed button long-press timer
+
+bool active_Loop = false;         // loop button press active?
+bool active_Loop_LP = false;      // loop button long-press active?
+long timer_Loop = 0;              // loop button long-press timer
+
+bool active_Back = false;         // back button press active?
+bool active_Back_LP = false;      // back button long-press active?
+long timer_Back = 0;              // back button long-press timer
+
+bool active_PlayPause = false;    // play/pause button press active?
+bool active_PlayPause_LP = false; // play/pause button long-press active?
+long timer_PlayPause = 0;         // play/pause button long-press timer
+
+bool active_Forward = false;      // forward button press active?
+bool active_Forward_LP = false;   // forward button long-press active?
+long timer_Forward = 0;           // forward button long-press timer
+
+
+/* ----------------------------------------------------------------------------------------------
+   press and release a combination of keys via USB HID
+   ----------------------------------------------------------------------------------------------
+*/
+void pressKeys(bool ctrl, bool shift, bool alt, char c) {
+  // press ctrl if necessary
+  if (ctrl) {
+    Keyboard.press(KEY_LEFT_CTRL);
+  }
+
+  // press shift if necessary
+  if (shift) {
+    Keyboard.press(KEY_LEFT_SHIFT);
+  }
+
+  // press alt if necessary
+  if (alt) {
+    Keyboard.press(KEY_LEFT_ALT);
+  }
+
+  // press character and release after 100ms
+  Keyboard.press(c);
+  delay(100);
+  Keyboard.release(c);
+
+  // release all other keys
+  Keyboard.releaseAll();
+}
+
+
+/* ----------------------------------------------------------------------------------------------
+   setup
+   ----------------------------------------------------------------------------------------------
+*/
+void setup() {
+  // setup pins as inputs
+  pinMode(button_Speed, INPUT);
+  pinMode(button_Loop, INPUT);
+  pinMode(button_Back, INPUT);
+  pinMode(button_PlayPause, INPUT);
+  pinMode(button_Forward, INPUT);
+
+  // start USB HID interface
+  Keyboard.begin();
+}
+
+
+/* ----------------------------------------------------------------------------------------------
+   loop
+   ----------------------------------------------------------------------------------------------
+*/
+void loop() {
+
+  /* ----------------------------------------------------------------------------------------------
+     BUTTON: SPEED
+     ----------------------------------------------------------------------------------------------
+  */
+
+  // button pressed
+  if (digitalRead(button_Speed) == HIGH) {
+
+    // has not been pressed before => start timer
+    if (active_Speed == false) {
+      active_Speed = true;
+      timer_Speed = millis();
+    }
+
+    // this is a long press
+    if ((millis() - timer_Speed > longPressTime) && (active_Speed_LP == false)) {
+      active_Speed_LP = true;
+      // do long-press action: nothing
+    }
+  }
+
+  // button not pressed anymore
+  else if (active_Speed == true) {
+    active_Speed = false;
+
+    // this was a short press
+    if (active_Speed_LP == false) {
+      // do short-press action: Shift+Alt+E
+      pressKeys(false, true, true, 'e');
+    }
+
+    // this was a long press
+    else {
+      active_Speed_LP = false;
+    }
+  }
+
+
+  /* ----------------------------------------------------------------------------------------------
+     BUTTON: LOOP
+     ----------------------------------------------------------------------------------------------
+  */
+
+  // button pressed
+  if (digitalRead(button_Loop) == HIGH) {
+
+    // has not been pressed before => start timer
+    if (active_Loop == false) {
+      active_Loop = true;
+      timer_Loop = millis();
+    }
+
+    // this is a long press
+    if ((millis() - timer_Loop > longPressTime) && (active_Loop_LP == false)) {
+      active_Loop_LP = true;
+      // do long-press action: nothing
+    }
+  }
+
+  // button not pressed anymore
+  else if (active_Loop == true) {
+    active_Loop = false;
+
+    // this was a short press
+    if (active_Loop_LP == false) {
+      // do short-press action: Shift+Alt+W
+      pressKeys(false, true, true, 'w');
+    }
+
+    // this was a long press
+    else {
+      active_Loop_LP = false;
+    }
+  }
+
+
+  /* ----------------------------------------------------------------------------------------------
+     BUTTON: BACK
+     ----------------------------------------------------------------------------------------------
+  */
+
+  // button pressed
+  if (digitalRead(button_Back) == HIGH) {
+
+    // has not been pressed before => start timer
+    if (active_Back == false) {
+      active_Back = true;
+      timer_Back = millis();
+    }
+
+    // this is a long press
+    if ((millis() - timer_Back > longPressTime) && (active_Back_LP == false)) {
+      active_Back_LP = true;
+      // do long-press action: Shift+Alt+R
+      pressKeys(false, true, true, 'r');
+    }
+  }
+
+  // button not pressed anymore
+  else if (active_Back == true) {
+    active_Back = false;
+
+    // this was a short press
+    if (active_Back_LP == false) {
+      // do short-press action: Shift+Alt+A
+      pressKeys(false, true, true, 'a');
+    }
+
+    // this was a long press
+    else {
+      active_Back_LP = false;
+    }
+  }
+
+
+  /* ----------------------------------------------------------------------------------------------
+     BUTTON: PLAY/PAUSE
+     ----------------------------------------------------------------------------------------------
+  */
+
+  // button pressed
+  if (digitalRead(button_PlayPause) == HIGH) {
+
+    // has not been pressed before => start timer
+    if (active_PlayPause == false) {
+      active_PlayPause = true;
+      timer_PlayPause = millis();
+    }
+
+    // this is a long press
+    if ((millis() - timer_PlayPause > longPressTime) && (active_PlayPause_LP == false)) {
+      active_PlayPause_LP = true;
+      // do long-press action: nothing
+    }
+  }
+
+  // button not pressed anymore
+  else if (active_PlayPause == true) {
+    active_PlayPause = false;
+
+    // this was a short press
+    if (active_PlayPause_LP == false) {
+      // do short-press action: Shift+Alt+S
+      pressKeys(false, true, true, 's');
+    }
+
+    // this was a long press
+    else {
+      active_PlayPause_LP = false;
+    }
+  }
+
+
+  /* ----------------------------------------------------------------------------------------------
+     BUTTON: FORWARD
+     ----------------------------------------------------------------------------------------------
+  */
+
+  // button pressed
+  if (digitalRead(button_Forward) == HIGH) {
+
+    // has not been pressed before => start timer
+    if (active_Forward == false) {
+      active_Forward = true;
+      timer_Forward = millis();
+    }
+
+    // this is a long press
+    if ((millis() - timer_Forward > longPressTime) && (active_Forward_LP == false)) {
+      active_Forward_LP = true;
+      // do long-press action: nothing
+    }
+  }
+
+  // button not pressed anymore
+  else if (active_Forward == true) {
+    active_Forward = false;
+
+    // this was a short press
+    if (active_Forward_LP == false) {
+      // do short-press action: Shift+Alt+D
+      pressKeys(false, true, true, 'd');
+    }
+
+    // this was a long press
+    else {
+      active_Forward_LP = false;
+    }
+  }
+
+
+}
---
+++ Tampermonkey/OVLP.user.js
@@ -0,0 +1,294 @@
+// ==UserScript==
+// @name         Online Video Looper Pedal
+// @version      0.1
+// @description  handsfree looping in Youtube and Vimeo
+// @author       Andreas Brett
+// @match        *://*.youtube.com/*
+// @match        *://*.vimeo.com/*
+// @exclude      *://*.youtube.com/subscribe_embed?*
+// @run-at       document-idle
+// ==/UserScript==
+
+
+
+/* ========================================================================================
+   CUSTOMIZE KEY MAPPINGS
+    - key: any character (a-z) or digit (0-9)
+    - possible modifiers: "ctrl", "shift", "alt"
+    ----> combine modifiers by concatenating them (e.g. "shift,alt")
+    ----> note: combining ctrl and shift won't work unfortunately
+======================================================================================== */
+
+var button_Speed = {
+    "key": "e",
+    "modifiers": "shift,alt"
+}
+var button_Loop = {
+    "key" : "w",
+    "modifiers": "shift,alt"
+}
+var button_Back = {
+    "key": "a",
+    "modifiers": "shift,alt"
+}
+var button_PlayPause = {
+    "key": "s",
+    "modifiers": "shift,alt"
+}
+var button_Forward = {
+    "key": "d",
+    "modifiers": "shift,alt"
+}
+var button_Reset = {
+    "key": "q",
+    "modifiers": "shift,alt"
+}
+var button_Rewind = {
+    "key": "r",
+    "modifiers": "shift,alt"
+}
+
+
+
+
+
+/* ========================================================================================
+   DO NOT EDIT BELOW
+======================================================================================== */
+
+var ovlp_player = null;
+var ovlp_log = null;
+var ovlp_timer = null;
+var ovlp_loopStart = null;
+var ovlp_loopEnd = null;
+var ovlp_speed = 1.0;
+
+
+function log(s) {
+    // log to developer console
+    console.log("%cOnline Video Looper Pedal: %c" + s, "font-weight: bold; color: #990099", "color: gray");
+
+    // create log pane at first call
+    if (ovlp_log == null) createLogPane();
+
+    // show text to user
+    ovlp_log.innerHTML = s;
+    ovlp_log.style.display = "block";
+
+    // hide after 850msec
+    setTimeout(function() {
+        ovlp_log.innerHTML = "";
+        ovlp_log.style.display = "none";
+    }, 850);
+}
+
+function modifyButtonVar(b) {
+    // only use first character
+    b.key = b.key[0];
+
+    // check if letter or digit
+    if (/^[a-zA-Z]$/.test(b.key)) {
+        b.code = "Key" + b.key.toUpperCase();
+    } else if (/^[0-9]$/.test(b.key)) {
+        b.code = "Digit" + b.key();
+    } else {
+        b.code = null;
+    }
+
+    // split modifiers into array
+    b.modifiers = b.modifiers.split(",");
+    for (var i = 0; i < b.modifiers.length; i++) {
+        b.modifiers[i] = b.modifiers[i].toLowerCase() + "Key";
+    }
+}
+
+function checkInput(k, b) {
+    var bReturn = false;
+
+    // check if key has been depressed
+    if (k.code == b.code) {
+        bReturn = true;
+
+        // check if modifiers have also been depressed
+        for (var i = 0; i < b.modifiers.length; i++) {
+            bReturn = bReturn & k[b.modifiers[i]];
+        }
+    }
+
+    // fetch player element if not already loaded
+    if (bReturn && ovlp_player == null) {
+        ovlp_player = document.getElementsByTagName("video")[0];
+    }
+
+    return bReturn;
+}
+
+function handleKeyInput(k) {
+    // button "Speed"
+    if (checkInput(k, button_Speed)) {
+        k.preventDefault();
+        togglePlaybackRate();
+    }
+    // button "Loop"
+    else if (checkInput(k, button_Loop)) {
+        k.preventDefault();
+        setLoopMarkers();
+    }
+    // button "Back"
+    else if (checkInput(k, button_Back)) {
+        k.preventDefault();
+        seekBack();
+    }
+    // button "Play/Pause"
+    else if (checkInput(k, button_PlayPause)) {
+        k.preventDefault();
+        togglePlayPause();
+    }
+    // button "Forward"
+    else if (checkInput(k, button_Forward)) {
+        k.preventDefault();
+        seekForward();
+    }
+    // button "Reset"
+    else if (checkInput(k, button_Reset)) {
+        k.preventDefault();
+        clearSettings();
+    }
+
+    // button "Rewind"
+    else if (checkInput(k, button_Rewind)) {
+        k.preventDefault();
+        seekBegin();
+    }
+}
+
+function seekBegin() {
+    // seek back to beginning
+    ovlp_player.currentTime = 0;
+    log("Rewinding");
+}
+
+function seekBack() {
+    // seek back 5sec
+    ovlp_player.currentTime -= 5;
+    log("Rewinding 5sec");
+}
+
+function seekForward() {
+    // seek forward 5sec
+    ovlp_player.currentTime += 5;
+    log("Forwarding 5sec");
+}
+
+function togglePlaybackRate() {
+    // go through playback rate chain
+    if (ovlp_speed == 1) {
+        ovlp_speed = 0.75;
+    } else if (ovlp_speed == 0.75) {
+        ovlp_speed = 0.5;
+    } else if (ovlp_speed == 0.5) {
+        ovlp_speed = 0.35;
+    } else if (ovlp_speed == 0.5) {
+        ovlp_speed = 0.35;
+    } else if (ovlp_speed == 0.35) {
+        ovlp_speed = 0.25
+    } else {
+        ovlp_speed = 1.0;
+    }
+
+    // set playback rate
+    ovlp_player.playbackRate = ovlp_speed;
+    log("Playback rate = " + (ovlp_speed*100) + "%");
+}
+
+function togglePlayPause() {
+    // toggle play state
+    if (ovlp_player.paused) {
+        log("Play");
+        ovlp_player.play();
+    } else {
+        log("Pause");
+        ovlp_player.pause();
+    }
+}
+
+function setLoopMarkers() {
+    if (ovlp_loopStart == null) {
+        // set loop start marker
+        ovlp_loopStart = ovlp_player.currentTime;
+        log("Set loop start");
+    } else if (ovlp_loopEnd == null ) {
+        // set loop end marker
+        ovlp_loopEnd = ovlp_player.currentTime;
+        log("Set loop end");
+
+        // start loop checking
+        ovlp_timer = setInterval(checkPlayState, 250);
+    } else {
+        // clear loop markers
+        ovlp_loopStart = null;
+        ovlp_loopEnd = null;
+        log("Loop cleared");
+
+        // stop loop checking
+        clearInterval(ovlp_timer);
+    }
+}
+
+function clearSettings() {
+    log("Settings cleared");
+
+    // clear loop markers
+    ovlp_loopStart = null;
+    ovlp_loopEnd = null;
+
+    // reset playback rate to 100%
+    ovlp_speed = 1.0;
+    ovlp_player.playbackRate = 1.0;
+}
+
+function checkPlayState() {
+    if (ovlp_loopStart != null && ovlp_loopEnd != null) {
+        if (ovlp_player.currentTime >= ovlp_loopEnd) {
+            ovlp_player.currentTime = ovlp_loopStart;
+        }
+    }
+}
+
+function createLogPane() {
+    // calculate position
+    var rect = ovlp_player.getBoundingClientRect();
+    var width = 350;
+    var height = 35;
+    var left = rect.left + (rect.width - width)/2;
+    var top = rect.bottom - 2*height;
+
+    // create div element
+    var logDiv = document.createElement("div");
+    logDiv.id = "ovlp_log";
+    logDiv.innerHTML = "";
+    logDiv.style.cssText = "position: absolute; top: " + top + "px; left: " + left + "px; width: " + width + "px; height: " + height + "px; border-radius: 5px; margin: 0; padding: 0; background-color: #fff; font-weight: bold; font-size: " + (height*0.5) + "px; color: #333; text-align: center; line-height: " + height + "px; opacity: 0.8; display: none;";
+
+    // attach element to body
+    document.getElementsByTagName("body")[0].appendChild(logDiv);
+
+    // store reference to log pane
+    ovlp_log = document.getElementById("ovlp_log");
+}
+
+
+(function() {
+    'use strict';
+
+    // define key handlers
+    document.addEventListener("keyup", handleKeyInput);
+
+    // modify button vars
+    modifyButtonVar(button_Loop);
+    modifyButtonVar(button_Speed);
+    modifyButtonVar(button_Back);
+    modifyButtonVar(button_Forward);
+    modifyButtonVar(button_PlayPause);
+    modifyButtonVar(button_Reset);
+    modifyButtonVar(button_Rewind);
+})();
